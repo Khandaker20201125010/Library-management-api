519f886a9441a1939cbcb493e36fbe1d992a09d7
unknown <prantokih42@gmail.com>
2025-06-20 17:21:11 +0600

    mongodb connected

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
 import { Server } from "http";
 import app from "./app";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-
 let server: Server;
 
-const PORT = 5000;
+const PORT = 3000;
 
 async function main() {
-    try{
-        await mongoose.connect(process.env.MONGO_URL as string);
-        server = app.listen(PORT, () => {
-            console.log(`Server is listening on port ${PORT}`);
-        });
-    }catch(error){
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+      console.log("Database connected");
+    server = app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
